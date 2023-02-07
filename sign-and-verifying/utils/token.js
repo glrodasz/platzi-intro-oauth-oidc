66b0d28d23ecdacee47ed08b356c2ab1e24593ac
Guillermo Rodas <me@guillermorodas.com>
2023-02-07 21:38:23 +0100

    Verificando un JSON Web Token

--- sign-and-verifying/utils/token.js
+++ sign-and-verifying/utils/token.js
@@ -3,8 +3,11 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import jwt from "jsonwebtoken";
+import fs from "node:fs";
 
 const SECRET = process.env.SECRET;
+const PRIVATE_KEY_PATH = process.env.PRIVATE_KEY_PATH;
+const PUBLIC_KEY_PATH = process.env.PUBLIC_KEY_PATH;
 const ONE_MINUTE_IN_MILLISECONDS = 60 * 1000
 
 export const signToken = (user) => {
@@ -14,11 +17,20 @@ export const signToken = (user) => {
     exp: Date.now() + ONE_MINUTE_IN_MILLISECONDS,
   };
 
+  if(PRIVATE_KEY_PATH) {
+    const privateKey = fs.readFileSync(PRIVATE_KEY_PATH, 'utf8');
+    return jwt.sign(payload, privateKey, { algorithm: 'RS256' });
+  }
+
   return jwt.sign(payload, SECRET)
 };
 
 export const verifyToken = (token) => {
-  return null;
+  if(PUBLIC_KEY_PATH) {
+    const publicKey = fs.readFileSync(PUBLIC_KEY_PATH, 'utf8');
+    return jwt.verify(token, publicKey);
+  }
+  return jwt.verify(token, SECRET);
 };
 
 export const validateExpiration = (payload) => {
