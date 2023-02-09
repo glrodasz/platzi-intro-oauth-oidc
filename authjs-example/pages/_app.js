66f1e67f7617d4a1f75a27beea6b7116701066f2
Guillermo Rodas <me@guillermorodas.com>
2023-02-09 23:41:49 +0100

    Auth.js Example

--- authjs-example/pages/_app.js
+++ authjs-example/pages/_app.js
@@ -1,5 +1,13 @@
+import { SessionProvider } from "next-auth/react";
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
--- authjs-example/pages/index.js
+++ authjs-example/pages/index.js
@@ -1,9 +1,10 @@
+import { signIn, useSession } from "next-auth/react";
 import { ConnectButton } from "@/components/ConnectButton";
 import { MainContainer } from "@/components/MainContainer";
 import { Card } from "@/components/Card";
 
 export default function Home() {
-  const { data, status } = null
+  const { data, status } = useSession()
 
   return status === "authenticated" ? (
     <MainContainer>
@@ -11,7 +12,7 @@ export default function Home() {
     </MainContainer>
   ) : (
     <MainContainer>
-      <ConnectButton onClick={() => {}}>
+      <ConnectButton onClick={() => signIn("github")}>
         Login with GitHub
       </ConnectButton>
     </MainContainer>
