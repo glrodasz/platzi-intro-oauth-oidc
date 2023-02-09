281d70139345848e81f3bbc01b565ea3da70c58f
Guillermo Rodas <me@guillermorodas.com>
2023-02-09 20:22:55 +0100

    Resource Owner Password Flow

--- resource-owner-password-flow/pages/api/login.js
+++ resource-owner-password-flow/pages/api/login.js
@@ -11,7 +11,7 @@ const PASSWORD = process.env.PASSWORD;
 const AUTH0_TOKEN_URL = `https://${AUTH0_DOMAIN}/oauth/token`;
 
 export default async function handler(req, res) {
-  const scopes = [];
+  const scopes = ["read:sample"];
 
   const options = {
     method: "POST",
@@ -23,7 +23,9 @@ export default async function handler(req, res) {
       client_id: CLIENT_ID,
       client_secret: CLIENT_SECRET,
       scope: scopes.join(" "),
-      // FIXME: send audience, username, and password
+      audience: AUTH0_API_AUDIENCE,
+      username: USERNAME,
+      password: PASSWORD,
     }),
   };
 
